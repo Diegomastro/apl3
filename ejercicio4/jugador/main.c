23cb3bb061ad8842175c421d1b2ba948c55fb211
Marco Vitangeli <marco.vitangeli@avature.net>
2022-06-03 03:03:29 -0300

    prueba de comunicacion entre procesos y plan armado

--- ejercicio4/jugador/main.c
+++ ejercicio4/jugador/main.c
@@ -7,13 +7,13 @@ int main() {
     size_t size = 1024;
     int shmid;
     char* puntero_shm = NULL;
-    key_t key = ftok("/dev/shm/shm-mamo", 'x');
-
+    key_t key = ftok("/home/marco/sisop-apl/apl3/ejercicio4/test", 'x');
+    printf("%d\n", key);
     printf("IPC_CREAT = %d\n", IPC_CREAT);
-    shmid = shmget(key, size, 0);
-
+    shmid = shmget(key, size, 0644 | IPC_CREAT);
+    printf("%d\n", shmid);
     puntero_shm = shmat(shmid, NULL, 0);
-
+    printf("%s", puntero_shm);
     puts(puntero_shm);
     return 0;
 }
--- ejercicio4/main.c
+++ ejercicio4/main.c
@@ -5,12 +5,13 @@
 #include <string.h>
 
 int main() {
+    // execute as "sudo ./main if you are using /something"
     size_t len = 1024;
     int shmid = 0;
     char* addr = NULL;
     struct shmid_ds shmbuf; // para shmctl
-    key_t key = ftok("/dev/shm/shm-test", 'x');
-
+    key_t key = ftok("/home/marco/sisop-apl/apl3/ejercicio4/test", 'x');
+    printf("Key = %d\n", key);
     shmid = shmget(key, len, IPC_CREAT);
 
     printf("shmid = %d\n", shmid);
@@ -23,13 +24,14 @@ int main() {
     addr = shmat(shmid, NULL, 0);
 
     if ((int)addr == -1) {
-        puts("Addr es -1, error");
+        printf("%d\n", key);
+        puts("Addr == -1");
         return -2;
     }
 
+    printf("addr = %p\n", addr);
     strcpy(addr, "string escrito en memoria compartida");
-    printf("addr = %d\n", (int)addr);
-
+    
     while(1);
     return 0;
 }
