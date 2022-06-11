07721a33789f85c2ad38d3d3595044d8a0e1fa6b
Marco Vitangeli <marco.vitangeli@avature.net>
2022-06-10 21:55:36 -0300

    se agrega palabra de los jugadores

--- ejercicio4/main.c
+++ ejercicio4/main.c
@@ -4,8 +4,60 @@
 #include <sys/shm.h> 
 #include <string.h>
 #include <stdlib.h>
+#define MAX_PALABRA 21
+
+const char PALABRA_INICIAL[MAX_PALABRA] = "********************";
+
+char* crearMemoriaCantJugadores();
+char* crearMemoriaJugadores();
+char* conseguirPalabra();
+char* crearMemoriaJugador(char* path, char id);
+
+int main() {
+    char* cantJugadores = crearMemoriaCantJugadores(); 
+    char* vidasJugadores = crearMemoriaJugadores();
+    int numJugadores;
+    printf("%d", *cantJugadores);
+    printf("Ingrese el numero de jugadores\n");
+    scanf("%d", &numJugadores);
+    char estanTodos = 0;
+    puts("esperando que se conecten todos los jugadores");
+    while (numJugadores != *cantJugadores); //esperamos por la cantidad de jugadores
+    // PALABRA A ADIVINAR, despues habra que poner la logica para buscarlas de un archivo
+    char palabra[] = "palabra";
+    char* jugador1 = crearMemoriaJugador("/home/marco/sisop-apl/apl3/ejercicio4/jug_1", 'X');
+    char* jugador2 = crearMemoriaJugador("/home/marco/sisop-apl/apl3/ejercicio4/jug_2", 'Y');
+    char* jugador3 = crearMemoriaJugador("/home/marco/sisop-apl/apl3/ejercicio4/jug_3", 'Z');
+    return 0;
+}
 
 char* crearMemoriaJugadores() {
+    size_t len = sizeof(char) * 3;
+    int shmid = 0;
+    char* addr = NULL;
+    key_t key = ftok("/home/marco/sisop-apl/apl3/ejercicio4/vidas_jugadores", 'B');
+    shmid = shmget(key, len, IPC_CREAT);
+    addr = shmat(shmid, NULL, 0);
+
+    char vidasIniciales[] = {6 ,6 ,6};
+
+    memcpy(addr, vidasIniciales, len);    
+
+    return addr;
+}
+
+char* crearMemoriaJugador(char* path, char id) {
+    size_t len = MAX_PALABRA; // tam maximo de una palabra (sera una constante)
+    int shmid = 0;
+    char* addr = NULL;
+    key_t key = ftok(path, id);
+    shmid = shmget(key, len, IPC_CREAT);
+    addr = shmat(shmid, NULL, 0);
+    memcpy(addr, PALABRA_INICIAL, len);
+    return addr;
+}
+
+char* crearMemoriaCantJugadores() {
     size_t len = sizeof(char);
     int shmid = 0;
     char* addr = NULL;
@@ -18,17 +70,4 @@ char* crearMemoriaJugadores() {
     char x = 0;
     memcpy(addr, &x, sizeof(char)); 
     return addr;
-}
-
-int main() {
-    char* addr = crearMemoriaJugadores(); 
-    int numJugadores;
-    printf("%d", *addr);
-    printf("Ingrese el numero de jugadores\n");
-    scanf("%d", &numJugadores);
-    char estanTodos = 0;
-    puts("esperando que se conecten todos los jugadores");
-    while (numJugadores != *addr);
-    printf("fin del programa");
-    return 0;
 }
