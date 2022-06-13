f735922040d06b95ff62674b4a50e8ac527d15e9
Marco Vitangeli <marco.vitangeli@avature.net>
2022-06-13 00:23:20 -0300

    se agrega server basico

---
+++ ejercicio5/jugador/main.c
@@ -0,0 +1,10 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const* argv[]) {
+    return 0;
+}
--- ejercicio5/main.c
+++ ejercicio5/main.c
@@ -1,5 +1,36 @@
+#include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+#define MAX_JUGADORES 3
+int main(int argc, char const* argv[]) {
 
-int main(int argc, char** argv) {
+    int cantJugadores;
+    int socketsJugadores[MAX_JUGADORES] = {0};
+    puts("Ingrese la cantidad de jugadores:");
+    scanf("%d", &cantJugadores);
+
+    int server = socket(AF_LOCAL, SOCK_STREAM, 0);
+    struct sockaddr_in address;
+    address.sin_family = AF_LOCAL;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    bind(server, &address, sizeof(address));
+    listen(server, cantJugadores);
+
+    int jugadoresConectados = 0;
+    
+    // jugadores conectados == ingresados por teclado
+    while (jugadoresConectados != cantJugadores) {
+        int cliente = accept(server, &address, sizeof(address));
+        socketsJugadores[jugadoresConectados++] = cliente;
+    }
+
+    // while de partida
+     
+
     return 0;
 }
