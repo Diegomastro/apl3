ff3f3678af10184eee248bfbd37465951efaccff
Mastro <diegomastro99@gmail.com>
2022-06-13 00:58:50 -0300

    cliente conexion

--- ejercicio5/jugador/main.c
+++ ejercicio5/jugador/main.c
@@ -6,5 +6,40 @@
 #define PORT 8080
 
 int main(int argc, char const* argv[]) {
+    int sock = 0, valread, client;
+    struct sockaddr_in serv_addr;
+    char* hello = "Hello from client";
+    char buffer[1024] = { 0 };
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)
+        <= 0) {
+        printf(
+            "\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+ 
+    if ((client
+         = connect(sock, (struct sockaddr*)&serv_addr,
+                   sizeof(serv_addr)))
+        < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+
+    printf("reding\n");
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+ 
+    // closing the connected socket
+    close(client);
     return 0;
 }
--- ejercicio5/main.c
+++ ejercicio5/main.c
@@ -7,15 +7,15 @@
 #define PORT 8080
 #define MAX_JUGADORES 3
 int main(int argc, char const* argv[]) {
-
+    char* hello = "Hello from server";
     int cantJugadores;
     int socketsJugadores[MAX_JUGADORES] = {0};
     puts("Ingrese la cantidad de jugadores:");
     scanf("%d", &cantJugadores);
 
-    int server = socket(AF_LOCAL, SOCK_STREAM, 0);
+    int server = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
-    address.sin_family = AF_LOCAL;
+    address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
     bind(server, &address, sizeof(address));
@@ -25,10 +25,17 @@ int main(int argc, char const* argv[]) {
     
     // jugadores conectados == ingresados por teclado
     while (jugadoresConectados != cantJugadores) {
+        printf("Jugadores conectados = %d\n", jugadoresConectados);
         int cliente = accept(server, &address, sizeof(address));
         socketsJugadores[jugadoresConectados++] = cliente;
     }
 
+    for (int i = 0; i < jugadoresConectados; ++i)
+    {
+        send(socketsJugadores[i], hello, strlen(hello), 0);
+        fflush(stdout);
+    }
+    puts("yay");
     // while de partida
      
 
